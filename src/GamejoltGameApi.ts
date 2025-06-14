e8276168de53fc836bd61eb9690561d7f4f3e11e
Niklas Ekman <nikl.ekman@gmail.com>
2025-06-14 22:36:10 +0200

    Check for window

--- src/GamejoltGameApi.ts
+++ src/GamejoltGameApi.ts
@@ -10,11 +10,14 @@ export class GamejoltGameApi
 
     constructor(private_key: string, game_id: number, public readonly config: Config)
     {
-        const query_string = window.location.search;
-        const url_params = new URLSearchParams(query_string);
+        if (typeof window !== "undefined")
+        {
+            const query_string = window.location.search;
+            const url_params = new URLSearchParams(query_string);
 
-        this.token = url_params.get("gjapi_token");
-        this.username = url_params.get("gjapi_username");
+            this.token = url_params.get("gjapi_token");
+            this.username = url_params.get("gjapi_username");
+        }
 
         this.trophies = new GamejoltTrophiesApi(private_key, game_id, config);
         this.scores = new GamejoltScoresApi(private_key, game_id, config);
